2e8dba115df990bc47bde4f72620785f77b5221b
Mena <sandym3000@hotmail.com>
2022-04-14 11:46:26 -0500

    grid y javascript

---
+++ myScript.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
+
+function turnOnLight(){
+    document.getElementById("myImage").src =
+      "https://i.pinimg.com/originals/1a/ca/61/1aca61ab8b723da396dcede15c92eb43.jpg";
+    console.log("Light have turn on");
+    console.log(5+9);
+}
+
+function turnOffLight(){
+    document.getElementById("myImage").src =
+      "https://64.media.tumblr.com/ababa0dfa907d20342020fc7c8557af7/tumblr_o6dyl2UUEP1rzo8x9o1_500.gifv";
+}
